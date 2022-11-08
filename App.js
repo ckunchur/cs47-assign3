4cf15180ca93f1305ec8a5f22b86d2abd8905905
ckunchur <67437371+ckunchur@users.noreply.github.com>
2022-11-07 16:26:23 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -2,42 +2,42 @@ import { StyleSheet, SafeAreaView, Text, View, TouchableOpacity, Dimensions, Ima
 import { useSpotifyAuth } from "./utils";
 import {Themes, Images } from "./assets/Themes";
 import { AuthButton, TrackList } from './components';
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import {WebView} from "react-native-webview";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+import PreviewScreen from './components/PreviewScreen';
+import DetailsScreen from './components/DetailsScreen';
+import HomeScreen from './components/HomeScreen';
+const Stack = createStackNavigator();
+
+
+
+
 export default function App() {
   // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
-  const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
 
-  
-  
-  let header = <View style={styles.titleRow}>
-  <Image source={Images.spotify} style={styles.spotifyIcon}/>
-  <Text style={styles.titleText}>My Top Tracks</Text>
- </View>
-
-  // if token is True, render FlatList
-  // else, render Authentication button
-
-  let contentDisplayed; // set equal to list component if true
-  if (token) {
-    // render Flatlist
-    contentDisplayed = [header, <TrackList tracks={tracks}/>];
-  }
-  else { // if token false
-    // render AuthButton
-    contentDisplayed = 
-    <AuthButton authFunction={getSpotifyAuth}/>
-
-  }
   return (
-    <SafeAreaView style={styles.container}> 
-      {contentDisplayed} 
-    </SafeAreaView>
+       <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerTintColor: 'white', headerShadowVisible: false, headerStyle: { backgroundColor: Themes.colors.background }}}>
+          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}  />
+          <Stack.Screen name="DetailsScreen"  component={DetailsScreen} options={{title: 'Song details'}} />
+          <Stack.Screen name="PreviewScreen" component={PreviewScreen} options={{title: 'Song preview'}}  />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
+{/* <Stack.Navigator screenOptions={{headerShadowVisible: false}}>
+    <Stack.Screen name="Home"  component={HomeScreen} options={{headerShown: false}}/>
+    <Stack.Screen name="DetailsScreen"  component={DetailsScreen} 
+          options={{headerStyle: {backgroundColor: Themes.colors.background}, headerTintColor: 'white', title: 'Song details'}} />
+    <Stack.Screen name="PreviewScreen"  component={PreviewScreen} 
+          options={{headerStyle: {backgroundColor: Themes.colors.background}, headerTintColor: 'white', title: 'Song preview'}} />  
+</Stack.Navigator> */}
 
 const styles = StyleSheet.create({
   container: {
@@ -45,29 +45,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
-  },
-  titleRow: {
-    flexDirection: 'row',
-    padding: 20,
-    marginTop: 30,
-    justifyContent: 'center',
-    justifyContent: 'center',
-    alignItems: "center",
-    padding: 15
-
-  },
-  titleText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'white',
-    marginLeft: 20
-    
-  },
-  spotifyIcon: {
-    height: 0.08 * windowWidth,
-    width: 0.08 * windowWidth,
-    resizeMode: 'contain'
   },
   
 });
